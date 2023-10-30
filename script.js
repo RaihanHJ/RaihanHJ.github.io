ff206091cebae8ccd27e263b4929b1eb22c3f7bf
RaihanHJ <149303573+RaihanHJ@users.noreply.github.com>
2023-10-30 09:41:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+/*var typed = new Typed(".text", {
+    strings: ["fronted Developer", "Youtuber", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+*/
+
+var typed = new Typed('.text', {
+    strings: ['<i>Student Of</i> UIN SUSKA RIAU,', 'Program <i>Information System.</i> '],
+    typeSpeed: 50,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
+
+
+/*scroll section avtive link*/
+/*let sections = document.querySelectorAll('section');
+let navLink = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLink.forEach(sec => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+
+        };
+    });
+};*/
+
